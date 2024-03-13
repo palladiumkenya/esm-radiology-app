002d0f27c1e46f028d20a0ab81762992c249f154
jecihjoy <jecihjoy@gmail.com>
2024-03-13 23:29:16 +0300

    (feat) Update metrics endpoint

--- src/metrics-tiles/procedure-ordered-tile.component.tsx
+++ src/metrics-tiles/procedure-ordered-tile.component.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import SummaryTile from "../summary-tiles/summary-tile.component";
-import { useLabTestsStats } from "../summary-tiles/radiology-summary.resource";
+import { useProcedureOrderStats } from "../summary-tiles/radiology-summary.resource";
 
 const ProcedureOrderedTileComponent = () => {
   const { t } = useTranslation();
 
-  const { count: testOrderedCount } = useLabTestsStats("");
+  const { count: testOrderedCount } = useProcedureOrderStats("");
 
   return (
     <SummaryTile
-      label={t("orders", "Orders")}
+      label={t("orders", "Procedures")}
       value={testOrderedCount}
-      headerLabel={t("testsOrdered", "Tests ordered")}
+      headerLabel={t("procedureOrdered", "Pending Procedures")}
     />
   );
 };
