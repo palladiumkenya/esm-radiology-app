5e28cf0944b338117149674e92e1f82622a632ce
Jamie Arodi <51090527+arodidev@users.noreply.github.com>
2024-03-14 13:17:27 +0300

    (fix) search data tables by all colums (#32)
    * search bar searches by all columns
    
    * renaming import paths

--- src/hooks/useSearchResults.ts
+++ src/hooks/useSearchResults.ts
@@ -1,11 +1,29 @@
+import { formatDate, parseDate } from "@openmrs/esm-framework";
 import { useMemo } from "react";
+import { Result } from "../radiology-tabs/work-list/work-list.resource";
 
-export function useSearchResults<T>(data: T[], searchString: string): T[] {
+export function useSearchResults(data: Result[], searchString: string) {
+  const flattenedData = data.map((eachObject) => {
+    return {
+      ...eachObject,
+      id: eachObject.uuid,
+      date: formatDate(parseDate(eachObject.dateActivated)),
+      patient: eachObject.patient.display.split("-")[1],
+      orderNumber: eachObject.orderNumber,
+      accessionNumber: eachObject.accessionNumber,
+      procedure: eachObject.concept.display,
+      action: eachObject.action,
+      status: eachObject.fulfillerStatus ?? "--",
+      orderer: eachObject.orderer.display,
+      urgency: eachObject.urgency,
+    };
+  });
+
   const searchResults = useMemo(() => {
     if (searchString && searchString.trim() !== "") {
       const search = searchString.toLowerCase();
-      return data.filter((appointment) =>
-        Object.entries(appointment).some(([header, value]) => {
+      return flattenedData.filter((eachDataRow) =>
+        Object.entries(eachDataRow).some(([header, value]) => {
           if (header === "patientUuid") {
             return false;
           }
@@ -14,7 +32,7 @@ export function useSearchResults<T>(data: T[], searchString: string): T[] {
       );
     }
 
-    return data;
+    return flattenedData;
   }, [searchString, data]);
 
   return searchResults;
