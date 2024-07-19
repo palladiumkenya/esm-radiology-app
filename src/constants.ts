6ba0c6391bb2b175e5e9930dc38e530fdb74293c
felix <fkiprotich845@gmail.com>
2024-07-19 15:18:26 +0300

    Fix lint issues

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const moduleName = "@openmrs/esm-radiology-app";
-export const radiologyConceptClass_UUID= "8caa332c-efe4-4025-8b18-3398328e1323";
+export const radiologyConceptClass_UUID =
+  "8caa332c-efe4-4025-8b18-3398328e1323";
